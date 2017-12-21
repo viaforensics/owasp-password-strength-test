b92ffbc90a1a83ef2de2ec5a39427f5900077784
Stephan <stephantroyer@live.at>
2017-12-22 00:33:39 +0100

    Update definitions to match DefinitelyTyped propose

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
 export as namespace owaspPasswordStrengthTest;
-export = owaspPasswordStrengthTest;
 
-interface PasswordTestResult {
+export interface PasswordTestResult {
     /** error messages associated with the failed tests */
     errors: string[];
     /** enumerates which tests have failed, beginning from 0 with the first required test */
@@ -16,37 +15,53 @@ interface PasswordTestResult {
     isPassphrase: boolean;
     /** indicates whether or not the user's password satisfied the strength requirements */
     strong: boolean;
-    /**  indicates how many of the optional tests were passed. In order for the password to be considered "strong", it (by default) must either be a passphrase, or must pass a number of optional tests that is equal to or greater than configs.minOptionalTestsToPass */
+    /**
+     * indicates how many of the optional tests were passed;
+     * In order for the password to be considered "strong", it (by default) must either be a passphrase,
+     * or must pass a number of optional tests that is equal to or greater than configs.minOptionalTestsToPass
+     */
     optionalTestsPassed: number;
 }
-interface PasswordTestConfiguration {
-    /** Toggles the "passphrase" mechanism on and off. If set to false, the strength-checker will abandon the notion of "passphrases", and will subject all passwords to the same complexity requirements. */
+export interface PasswordTestConfiguration {
+    /**
+     * toggles the "passphrase" mechanism on and off;
+     * If set to false, the strength-checker will abandon the notion of "passphrases",
+     * and will subject all passwords to the same complexity requirements.
+     */
     allowPassphrases?: boolean;
     /** constraint on a password's maximum length */
     maxLength?: number;
     /** constraint on a password's minimum length */
     minLength?: number;
-    /** minimum length a password needs to achieve in order to be considered a "passphrase" (and thus exempted from the optional complexity tests by default) */
-    minPhraseLength?: number;
-    /** minimum number of optional tests that a password must pass in order to be considered "strong". By default (per the OWASP guidelines), four optional complexity tests are made, and a password must pass at least three of them in order to be considered "strong" */
-    minOptionalTestsToPass?: number;
     /**
-     * toggles the i18n error keys in place of english error messages.
-     * This can be useful when translating the errors using a 3rd party i18n library.
-     * When true the following keys can be returned:
-     * `failedMinLength`, `failedMaxLength`, `failedThreeRepeatedChars`, `optionalLowercaseRequired`, `optionalUppercaseRequired`, `optionalNumberRequired` and `optionalSpecialCharRequired`.
+     * minimum length a password needs to achieve in order to be considered a "passphrase"
+     * (and thus exempted from the optional complexity tests by default)
      */
-    i18nErrorKeys?: boolean;
+    minPhraseLength?: number;
+    /**
+     * minimum number of optional tests that a password must pass in order to be considered "strong";
+     * By default (per the OWASP guidelines), four optional complexity tests are made,
+     * and a password must pass at least three of them in order to be considered "strong"
+     */
+    minOptionalTestsToPass?: number;
+
+    // // to add support for https://github.com/nowsecure/owasp-password-strength-test/pull/5
+    // /**
+    //  * toggles the i18n error keys in place of english error messages.
+    //  * This can be useful when translating the errors using a 3rd party i18n library.
+    //  * When true the following keys can be returned:
+    //  * `failedMinLength`, `failedMaxLength`, `failedThreeRepeatedChars`, `optionalLowercaseRequired`, `optionalUppercaseRequired`, `optionalNumberRequired` and `optionalSpecialCharRequired`.
+    //  */
+    // i18nErrorKeys?: boolean;
 }
 
-type PasswordTest = (password: string) => string | undefined;
+export type PasswordTest = (password: string) => string | undefined;
 
-declare namespace owaspPasswordStrengthTest {
-    export function test(password: string): PasswordTestResult;
-    export function config(configuration: PasswordTestConfiguration): void;
+export function test(password: string): PasswordTestResult;
+export function config(configuration: PasswordTestConfiguration): void;
 
-    export let tests: {
-        required: PasswordTest[];
-        optional: PasswordTest[];
-    };
-}
+export let tests: {
+    required: PasswordTest[];
+    optional: PasswordTest[];
+};
+export const configs: Readonly<PasswordTestConfiguration>;
